fafe20a6266d55438ce1e0eacb9b5b0db4a8fb21
EndoShogo <tkg.gg.shogo@gmail.com>
2025-08-02 15:37:58 +0900

    Fix: Use python3 in build.sh for Vercel deployment

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
 # python manage.py migrate
