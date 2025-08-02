aa507334e69e9f18ed9921a2b4e2dcf0ba7c92ae
EndoShogo <tkg.gg.shogo@gmail.com>
2025-08-02 16:31:47 +0900

    fix: シグナルを使用してユーザー作成時にプロフィールを自動生成

--- reviewpost/apps.py
+++ reviewpost/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class ReviewpostConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviewpost'
+
+    def ready(self):
+        import reviewpost.models
--- reviewpost/models.py
+++ reviewpost/models.py
@@ -28,8 +28,17 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    def __str__(self):
-        return f"{self.user.username}のプロフィール"
+    from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
