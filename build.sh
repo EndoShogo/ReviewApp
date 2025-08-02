9a92c26f0cc3d80326b553f0416796875c8eeeec
EndoShogo <tkg.gg.shogo@gmail.com>
2025-08-02 15:34:04 +0900

    build.shのmigrateを一時的に無効化

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Build the project
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate
+set -e
+
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput
+# python manage.py migrate
