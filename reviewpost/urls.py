896148fe853413182b1ecb281bedd46d26b740d7
EndoShogo <tkg.gg.shogo@gmail.com>
2025-08-02 16:28:11 +0900

    hotfix: 破損したURL設定を緊急修復

--- reviewpost/urls.py
+++ reviewpost/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from .views import SignUpView, CustomLoginView, CustomLogoutView, listview,detailview,CreateClass,columnview,profileview,ProfileUpdateView,inboxview,message_detailview,send_messageview,like_review
+from .views import SignUpView, CustomLoginView, CustomLogoutView, listview, detailview, CreateClass, columnview, profileview, ProfileUpdateView, inboxview, message_detailview, send_messageview, like_review
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', listview, name='list'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('list/',listview , name='list'),
-    path('detail/<int:pk>',detailview,name='detail'),
-    path('create/',CreateClass.as_view(),name='create'),
+    path('detail/<int:pk>', detailview, name='detail'),
+    path('create/', CreateClass.as_view(), name='create'),
     path('column/', columnview, name='column'),
     path('profile/edit/', ProfileUpdateView.as_view(), name='profile_edit'),
     path('profile/<str:username>/', profileview, name='profile'),
@@ -17,4 +18,4 @@ urlpatterns = [
     path('send-message/', send_messageview, name='send_message'),
     path('send-message/<str:recipient_username>/', send_messageview, name='send_message_to_user'),
     path('like/<int:review_id>/', like_review, name='like_review'),
-] +static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
