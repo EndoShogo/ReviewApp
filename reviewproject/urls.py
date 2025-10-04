e26fdbd339d1af3fe9be429f2d2bad6c4d36ee18
EndoShogo <tkg.gg.shogo@gmail.com>
2025-10-04 15:06:20 +0900

    Fix favicon.ico 404 error by adding URL routing and inline favicon

--- reviewproject/urls.py
+++ reviewproject/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.views.generic import RedirectView
+
+def favicon_view(request):
+    # 空のレスポンスを返してfavicon.icoリクエストを処理
+    return HttpResponse(status=204)
 
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',include('reviewpost.urls')),
+    path('favicon.ico', favicon_view, name='favicon'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
